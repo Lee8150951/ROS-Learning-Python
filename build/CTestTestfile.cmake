86c6fad18d10b0c6667300b8634a6422b6117a6c
Lee8150951 <Liys9875@outlook.com>
2021-08-02 21:10:51 +0800

    topic-model

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jacob/Desktop/ROSWorkspace-Python/src
+# Build directory: /home/jacob/Desktop/ROSWorkspace-Python/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("topic")
